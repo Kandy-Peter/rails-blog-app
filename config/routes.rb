b90597f4afba015e2b687073af843f210403d895
kandy-peter <kandypeter03@gmail.com>
2022-05-11 17:25:26 +0200

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "users#index"
+  get 'users' => "users#index"
   get 'users/:id' => 'users#show'
   get 'users/:id/posts'=> 'posts#index'
   get 'users/:id/posts/:post_id' => 'posts#show'
