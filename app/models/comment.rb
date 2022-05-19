443f4640c459039f2e5d226ab01380cc36edc11d
kandy-peter <kandypeter03@gmail.com>
2022-05-19 14:48:26 +0200

    create custom methods for tables

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_comment_counter
+    count = Comment.where(post_id: id).count
+    post = Post.find_by_id(id: post_id)
+    post.update(comments_counter: count)
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_like_counter
+    count = Comment.where(post_id: id).count
+    post = Post.find_by_id(id: post_id)
+    post.update(likes_counter: count)
 end
