47196c0870f4c520fb90a1b59e6e433c81da7f15
kandy-peter <kandypeter03@gmail.com>
2022-05-19 14:19:31 +0200

    add method for three recents posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def recent_posts
+    Post.order(created_at: :desc).where(author: id).first(3)
+  end
 end
