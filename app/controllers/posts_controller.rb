70e07ad8d692d6deb770d84ce620e97438dcd20d
kandy-peter <kandypeter03@gmail.com>
2022-05-20 12:25:13 +0200

    create link between posts and posts/:id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: current_user)
-    @posts = Post.where(author_id: current_user)
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   end
 
   def show
