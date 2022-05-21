0df196b203c7743a84f9641586413a37805d3b30
kandy-peter <kandypeter03@gmail.com>
2022-05-21 16:38:53 +0200

    Add units specs for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'check likes model' do
+    before(:each) do
+      @like = Like.new(author_id: 6, post_id: 8)
+    end
+
+    before { @like.save }
+
+    it 'if author_id is present' do
+      @like.author_id = false
+      expect(@like).to_not be_valid
+    end
+
+    it 'if post_id is present' do
+      @like.post_id = nil
+      @like.author_id = nil
+      expect(@like).to_not be_valid
+    end
+  end
 end
