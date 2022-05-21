925ebed0d45b7cf479a1f803d9bce32b4cd7306b
kandy-peter <kandypeter03@gmail.com>
2022-05-21 16:58:55 +0200

    Add unit spec for Like and Comment models

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   describe 'check likes model' do
     before(:each) do
-      @like = Like.new(author_id: 6, post_id: 8)
+      @like = Like.new(author_id: 6, post_id: 1)
     end
 
     before { @like.save }
 
-    it 'if author_id is present' do
-      @like.author_id = false
-      expect(@like).to_not be_valid
+    it "author_id shouldn't be a string" do
+      subject.author_id = 'string'
+      expect(subject).to_not be_valid
     end
 
     it 'if post_id is present' do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  subject do
+  before(:each) do
     @user = User.new(name: 'Kandy', photo: 'https://microverse_student.com/img1', bio: 'software Developer form Congo')
-    Post.new(author: @user, title: 'post test', text: 'content for checking tests', comments_counter: 0, likes_counter: 0)
+    @post = Post.new(author: @user, title: 'post test', text: 'content for checking tests', comments_counter: 0, likes_counter: 0)
   end
-  before { subject.save }
+
+  before { @post.save }
 
   it 'title should be present' do
-    subject.title = nil
-    expect(subject).to_not be_valid
+    @post.title = nil
+    expect(@post).to_not be_valid
   end
 
   it 'title content max 250 characters' do
-    subject.title = "title"
-    expect(subject).to be_valid
+    @post.title = "title"
+    expect(@post).to be_valid
   end
 
   it 'test if likes counter is greater than or equal to 0' do
-    subject.likes_counter = -1
-    expect(subject).to_not be_valid
+    @post.likes_counter = -1
+    expect(@post).to_not be_valid
   end
 
   it 'test if likes counter is integer' do
-    subject.likes_counter = 3
-    expect(subject).to be_valid
+    @post.likes_counter = 3
+    expect(@post).to be_valid
   end
 end
