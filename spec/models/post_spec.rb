67f7a87784dd5c5ccb03cc43a7e21e43bced6df8
kandy-peter <kandypeter03@gmail.com>
2022-05-21 16:29:56 +0200

    Add unit test spec for POst model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    @user = User.new(name: 'Kandy', photo: 'https://microverse_student.com/img1', bio: 'software Developer form Congo')
+    Post.new(author: @user, title: 'post test', text: 'content for checking tests', comments_counter: 0, likes_counter: 0)
+  end
+  before { subject.save }
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'title content max 250 characters' do
+    subject.title = "title"
+    expect(subject).to be_valid
+  end
+
+  it 'test if likes counter is greater than or equal to 0' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
+
+  it 'test if likes counter is integer' do
+    subject.likes_counter = 3
+    expect(subject).to be_valid
+  end
 end
